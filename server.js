a48c57eb869816445362c722646974303c658e43
Sandeep Kumar Jha <kumarjha.sandeep@gmail.com>
2016-10-06 22:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res){
-   res.send(cerateTemplate(articleOne));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res){
