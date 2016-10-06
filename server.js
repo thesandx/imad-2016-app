de4b4fc0d8493e28b4adbb5da5ea7155c8613049
Sandeep Kumar Jha <kumarjha.sandeep@gmail.com>
2016-10-06 19:55:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two',function (req, res){
 });
 
 app.get('/article-three',function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'inarticle-three.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
