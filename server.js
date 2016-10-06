07c9c3206fb962653ab0a2f3d8ef8b621bcbcc1d
Sandeep Kumar Jha <kumarjha.sandeep@gmail.com>
2016-10-06 20:23:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+title:'Article One | TheSandX',    
+ heading:'Article-one',
+ date:'Oct 5 , 2016',
+ content:` <p>
+             This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+         </p>
+         
+          <p>
+             This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+         </p>
+         
+         <p>
+             This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+         </p>`
+    
+    
+    
+    
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
