f2e7372f9b2ca8310393292479c033f0beae192a
Sandeep Kumar Jha <kumarjha.sandeep@gmail.com>
2016-10-09 11:05:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ var counter = 0;
 
 app.get('/counter',function(req,res){
     counter+=1;
-    res.send(counter.toString(radix));
+    res.send(counter.toString());
 });
 
 app.get('/:articleName',function (req, res){
